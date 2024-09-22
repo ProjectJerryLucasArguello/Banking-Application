d6f71b12a15737656092585464017e717655854e
Jerry Arguello <jla44@njit.edu>
2024-09-22 19:45:09 -0400

    init commit

--- the-banking-application/the-banking-application/src/main/java/com/argjerryl/the_banking_application/controller/TransactionController.java
+++ the-banking-application/the-banking-application/src/main/java/com/argjerryl/the_banking_application/controller/TransactionController.java
@@ -1,6 +1,7 @@
 package com.argjerryl.the_banking_application.controller;
 import com.argjerryl.the_banking_application.enitity.Transaction;
 import com.argjerryl.the_banking_application.service.BankStatement;
+import com.itextpdf.text.DocumentException;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileNotFoundException;
 import java.util.List;
 
 @RestController
@@ -22,7 +24,7 @@ public class TransactionController {
     public List<Transaction> generateBankStatement(@RequestParam String accountNumber,
                                                    @RequestParam String startDate,
                                                    @RequestParam String endDate
-                                                   ){
+                                                   ) throws DocumentException, FileNotFoundException {
         return bankStatement.generateStatement(accountNumber, startDate, endDate);
     }
 }
--- the-banking-application/the-banking-application/src/main/java/com/argjerryl/the_banking_application/service/BankStatement.java
+++ the-banking-application/the-banking-application/src/main/java/com/argjerryl/the_banking_application/service/BankStatement.java
@@ -1,6 +1,8 @@
 package com.argjerryl.the_banking_application.service;
 
+import com.argjerryl.the_banking_application.enitity.AccountInfo;
 import com.argjerryl.the_banking_application.enitity.Transaction;
+import com.argjerryl.the_banking_application.repository.AccountRepository;
 import com.argjerryl.the_banking_application.repository.TransactionRepository;
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfPCell;
@@ -10,6 +12,7 @@ import lombok.AllArgsConstructor;
 //import org.springframework.cglib.core.Local;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.FileNotFoundException;
@@ -35,15 +38,24 @@ public class BankStatement {
      * send via email
      */
 
+    @Autowired
+    AccountRepository accountRepository;
+
     //Checking the accountNumber and it's action between a certain time frame
     public List<Transaction> generateStatement(String accountNumber, String startDate, String endDate) throws FileNotFoundException, DocumentException {
         //parse() takes a string and manipulates it into the type of variable the creator wants
         LocalDate start = LocalDate.parse(startDate, DateTimeFormatter.ISO_DATE);
         LocalDate end = LocalDate.parse(endDate, DateTimeFormatter.ISO_DATE);
 
-
+        List<Transaction> transactionList = transactionRepository.findAll().stream().filter(transaction -> transaction.getAccountNumber().equals(accountNumber))
+                .filter(transaction -> transaction.getCreatedAt().isEqual(start))
+                .filter(transaction -> transaction.getCreatedAt().isEqual(end)).toList();
         //Design the size of account statement
         //Provide the file
+
+        AccountInfo user = accountRepository.findByAccountNumber(accountNumber);
+        String customerName = user.getFirstName() + " " + user.getLastName();
+
 
         Rectangle statementSize = new Rectangle(PageSize.A4); // Unreachable statement
         Document document = new Document(statementSize);
@@ -64,16 +76,61 @@ public class BankStatement {
         bankInfoTable.addCell(bankAddress);
 
         //=========================================//
-
+        //Added all these cells into the statmentInfo
         PdfPTable statementInfo = new PdfPTable(2);
         PdfPCell customerInfo = new PdfPCell(new Phrase("Start Date: " + startDate));
         customerInfo.setBorder(0);
         PdfPCell statement = new PdfPCell(new Phrase("STATEMENT OF ACCOUNT"));
         statement.setBorder(0);
         PdfPCell stopDate = new PdfPCell(new Phrase("End Date: " + endDate));
+        stopDate.setBorder(0);
 
-        return transactionRepository.findAll().stream().filter(transaction -> transaction.getAccountNumber().equals(accountNumber))
-                .filter(transaction -> transaction.getCreatedAt().isEqual(start))
-                .filter(transaction -> transaction.getCreatedAt().isEqual(end)).toList();
+        PdfPCell name = new PdfPCell(new Phrase("Customer Name: " + customerName));
+        name.setBorder(0);
+        PdfPCell space = new PdfPCell();
+        space.setBorder(0);
+        PdfPCell address = new PdfPCell(new Phrase("Customer Address: " + user.getAddress()));
+        address.setBorder(0);
+
+        PdfPTable transactionTable = new PdfPTable(4);
+        PdfPCell date = new PdfPCell(new Phrase("DATE"));
+        date.setBackgroundColor(BaseColor.GREEN);
+        date.setBorder(0);
+        PdfPCell transactionType = new PdfPCell(new Phrase("TRANSACTION TYPE"));
+        transactionType.setBackgroundColor(BaseColor.GREEN);
+        transactionType.setBorder(0);
+        PdfPCell transactionAmount = new PdfPCell(new Phrase("TRANSACTION AMOUNT"));
+        transactionAmount.setBackgroundColor(BaseColor.GREEN);
+        transactionAmount.setBorder(0);
+        PdfPCell status = new PdfPCell(new Phrase("STATUS"));
+        status.setBackgroundColor(BaseColor.GREEN);
+        status.setBorder(0);
+
+        transactionTable.addCell(date);
+        transactionTable.addCell(transactionType);
+        transactionTable.addCell(transactionAmount);
+        transactionTable.addCell(status);
+
+        transactionList.forEach(transaction -> {
+            transactionTable.addCell(new Phrase(transaction.getCreatedAt().toString()));
+            transactionTable.addCell(new Phrase(transaction.getTransactionType()));
+            transactionTable.addCell(new Phrase(transaction.getAmount().toString()));
+            transactionTable.addCell(new Phrase(transaction.getStatus()));
+        });
+
+        statementInfo.addCell(customerInfo);
+        statementInfo.addCell(statement);
+        statementInfo.addCell(endDate);
+        statementInfo.addCell(name);
+        statementInfo.addCell(space);
+        statementInfo.addCell(address);
+
+        document.add(bankInfoTable);
+        document.add(statementInfo);
+        document.add(transactionTable);
+
+        document.close();
+
+        return transactionList;
     }
 }
